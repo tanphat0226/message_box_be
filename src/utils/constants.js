6903ecd1a96fd7cd3e509bb1ea362b51b9640daa
ryanpham <ryanphamit@gmail.com>
2025-03-01 20:58:47 +0700

    Add additional domains to the whitelist

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,6 @@
-export const WHITELIST_DOMAINS = ['http://localhost:5173']
+export const WHITELIST_DOMAINS = [
+  'http://localhost:5173',
+  'https://inspire-box.vercel.app',
+  'https://inspire-box-ryanpham0226-project.vercel.app',
+  'https://inspire-box-git-main-ryanpham0226-project.vercel.app'
+]
